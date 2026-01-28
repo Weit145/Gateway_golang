da77b4fc0e5a08f53e34b7beb8e0f98e785599a1
Weit145 <weit.dima@yandex.ru>
2026-01-28 17:02:48 +0300

    Add confirm grpc with test :alien:

--- internal/http-server/handler/confirm/confirm.go
+++ internal/http-server/handler/confirm/confirm.go
@@ -1,11 +1,14 @@
 package confirm
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 	"time"
 
+	"github.com/Weit145/GATEWAY_golang/internal/lib/logger"
 	"github.com/Weit145/GATEWAY_golang/internal/lib/response"
+	GRPCauth "github.com/Weit145/proto-repo/auth"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
@@ -15,14 +18,16 @@ type Request struct {
 	TokenEmail string
 }
 
+//go:generate go run github.com/vektra/mockery/v2@v2.53.5 --name=GRPCConfirmUser
 type GRPCConfirmUser interface {
+	RegistrationUser(ctx context.Context, token string) (*GRPCauth.CookieResponse, error)
 }
 
 type Response struct {
 	TokenAsset string
 }
 
-func New(log *slog.Logger) http.HandlerFunc {
+func New(log *slog.Logger, grpcConfirm GRPCConfirmUser) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		const op = "http-server.handler.confirm.New"
 
@@ -43,20 +48,30 @@ func New(log *slog.Logger) http.HandlerFunc {
 			return
 		}
 
+		resp, err := grpcConfirm.RegistrationUser(r.Context(), token)
+		if err != nil {
+			log.Error("Failed confirm user", logger.Err(err))
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error(
+				"Failed confirm user",
+			))
+			return
+		}
+
 		http.SetCookie(w, &http.Cookie{
-			Name:     "refresh_token",
-			Value:    "abc123",
+			Name:     resp.Cookie.Key,
+			Value:    resp.Cookie.Value,
 			Path:     "/", // доступна всему сайту
-			Expires:  time.Now().Add(24 * time.Hour),
-			HttpOnly: false,                // теперь можно видеть в браузере через JS
-			Secure:   false,                // локально без HTTPS
+			Expires:  time.Now().Add(time.Duration(resp.Cookie.MaxAge) * time.Hour),
+			HttpOnly: resp.Cookie.Httponly, // теперь можно видеть в браузере через JS
+			Secure:   resp.Cookie.Secure,   // локально без HTTPS
 			SameSite: http.SameSiteLaxMode, // Lax позволит отправлять cookie на GET запросы
 		})
 
-		var resp Response
-		resp.TokenAsset = "abc123"
+		var out Response
+		out.TokenAsset = resp.AccessToken
 
-		w.Header().Set("Authorization", "Bearer "+resp.TokenAsset)
+		w.Header().Set("Authorization", "Bearer "+out.TokenAsset)
 		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 
 		render.Status(r, http.StatusOK)
--- internal/http-server/handler/confirm/confirm_test.go
+++ internal/http-server/handler/confirm/confirm_test.go
@@ -2,29 +2,59 @@ package confirm_test
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
+	grpc "github.com/Weit145/proto-repo/auth"
+
 	"github.com/Weit145/GATEWAY_golang/internal/http-server/handler/confirm"
+	"github.com/Weit145/GATEWAY_golang/internal/http-server/handler/confirm/mocks"
 	"github.com/Weit145/GATEWAY_golang/internal/lib/logger/slogdiscard"
 	"github.com/Weit145/GATEWAY_golang/internal/lib/response"
 	"github.com/go-chi/chi"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
 func TestConfirmHandler(t *testing.T) {
+	CookieResponse := &grpc.CookieResponse{
+		AccessToken: "123123",
+		Cookie: &grpc.Cookie{
+			Key:      "refresh_token",
+			Value:    "",
+			Httponly: true,
+			Secure:   true,
+			Samesite: "",
+			MaxAge:   24,
+		},
+	}
+
 	cases := []struct {
-		name      string
-		token     string
-		respError string
-		mockError error
+		name       string
+		token      string
+		respError  string
+		mockError  error
+		mockResult *grpc.CookieResponse
+		shouldCall bool
 	}{
 		{
-			name:      "Success",
-			token:     "1231241231",
-			respError: "success",
+			name:       "Success",
+			token:      "1231241231",
+			respError:  "success",
+			mockError:  nil,
+			mockResult: CookieResponse,
+			shouldCall: true,
+		},
+		{
+			name:       "Error confirm user",
+			token:      "1231241231",
+			respError:  "error",
+			mockError:  errors.New("Failed to confirm"),
+			mockResult: nil,
+			shouldCall: true,
 		},
 	}
 
@@ -33,14 +63,26 @@ func TestConfirmHandler(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			handler := confirm.New(slogdiscard.NewDiscardLogger())
+			mockConfirmUser := mocks.NewGRPCConfirmUser(t)
+
+			if tc.shouldCall {
+				mockConfirmUser.On("RegistrationUser", mock.Anything, tc.token).
+					Return(tc.mockResult, tc.mockError).
+					Once()
+			}
+
+			handler := confirm.New(slogdiscard.NewDiscardLogger(), mockConfirmUser)
 
 			req := httptest.NewRequest(http.MethodGet, "/registration/confirm/"+tc.token, nil)
 
 			r := chi.NewRouter()
-			r.Get("/registration/confirm/{token}", handler) // маршрут с параметром
+			r.Get("/registration/confirm/{token}", handler)
 			rr := httptest.NewRecorder()
 			r.ServeHTTP(rr, req)
+
+			var resp response.Response
+			require.NoError(t, json.Unmarshal(rr.Body.Bytes(), &resp))
+			require.Equal(t, tc.respError, resp.Status)
 
 			if tc.respError != "error" {
 				cookies := rr.Result().Cookies()
@@ -59,9 +101,6 @@ func TestConfirmHandler(t *testing.T) {
 				assetToken := rr.Result().Header.Get("Authorization")
 				require.True(t, strings.HasPrefix(assetToken, "Bearer "))
 			}
-			var resp response.Response
-			require.NoError(t, json.Unmarshal(rr.Body.Bytes(), &resp))
-			require.Equal(t, tc.respError, resp.Status)
 		})
 	}
 }
